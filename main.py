8383618d7e7fae5423841b0f1ce7ff568395b30d
ivo100 <ivostoy@gmail.com>
2025-03-29 13:01:23 -0700

    initial

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press ⌥⇧R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌃' to toggle the breakpoint.
+# pip install python-dotenv
+import dotenv
+from icecream import ic
 
 
-# Press the green button in the gutter to run the script.
+
+def example():
+    ic(acct)
+    ic(token)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dotenv.load_dotenv()
+    acct = os.getenv(f"gemini_acct")
+    token = os.getenv(f"gemini_token")
+    assert acct
+    assert token
+    example()
